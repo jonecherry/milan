c030c186d3059a1c2aae2884cde4f4c1f7230b01
jonecherry <junchen.unif@qq.com>
2016-12-06 21:55:55 +0800

    footer.blade.php

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,46 +1,51 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-4 col-sm-4 col-md-4 menu">
-			<?php $menu=\Menu::help(true); ?>
-			<h3>{{ trans('globals.company_label') }}</h3>
+			<h3>米兰居家</h3>
 			<ul>
-				@foreach ($menu as $item)
-					<li><a href = '{{ $item['route'] }}'>{{ $item['text'] }}</a></li>
-				@endforeach
+				<li>电话 ：18606797079</li>
+				<li>地址 ：浙江省 东阳市 通江路140号（锦豪大酒店斜对面）</li>
+				<li>主要经营 ：窗帘、壁纸、软包</li>
 			</ul>
+		</div>
+		<div class="col-xs-4 col-sm-4 col-md-4 menu" >
+			<h3>米兰居家的微信公众号</h3>
+			<img  src='/img/qrcode_milan.jpg'>
+
 		</div>
 
 		<div class="col-xs-4 col-sm-4 col-md-4 menu">
-			<h3>{{ trans('globals.social_label') }}</h3>
+			<h3>米兰居家的合作平台</h3>
 			<ul>
-				<li><a href="https://www.facebook.com/{{ $main_company['facebook'] }}" target="_blank">{{ trans('globals.facebook_label') }}</a></li>
-				<li><a href="https://twitter.com/{{ $main_company['twitter'] }}" target="_blank">{{ trans('globals.twitter_label') }}</a></li>
-				<li><a href="https://plus.google.com/u/0/{{ $main_company['google_plus'] }}" target="_blank">{{ trans('globals.google_label') }}</a></li>
+				<li><a href="https://www.taobao.com" target="_blank">淘宝</a></li>
+				<li><a href="https://weidian.com/" target="_blank">微店</a></li>
+				{{--<li><a href="https://plus.google.com/u/0/{{ $main_company['google_plus'] }}" target="_blank">{{ trans('globals.google_label') }}</a></li>--}}
 			</ul>
 		</div>
 
-		<div class="col-xs-4 col-sm-4 col-md-4 newsletter" ng-controller = "NewslettersCtrl">
-			@if (\Auth::user())
-				<p>{{ trans('globals.reach_us_msg') }}</p>
-				<p><strong><a href="/contact"><span class="glyphicon glyphicon-envelope"></span>&nbsp;{{ trans('globals.send_a_email_label') }}</a></strong></p>
-			@else
-				<div class="signup clearfix">
-					<p>{{ trans('user.newsletter_sign_up') }}</p>
-					<form>
-						<input type="text" ng-model = "newsEmail"  class = "form-control input-sm" placeholder = "{{ trans('user.your_email_address_label') }}">
-						<input type="button" ng-click = "save()" value = "{{ trans('user.sign_up_label') }}">
-					</form>
-				</div>
-			@endif
-		</div>
+		{{--<div class="col-xs-4 col-sm-4 col-md-4 newsletter" ng-controller = "NewslettersCtrl">--}}
+			{{--@if (\Auth::user())--}}
+				{{--<p>{{ trans('globals.reach_us_msg') }}</p>--}}
+				{{--<p><strong><a href="/contact"><span class="glyphicon glyphicon-envelope"></span>&nbsp;{{ trans('globals.send_a_email_label') }}</a></strong></p>--}}
+			{{--@else--}}
+				{{--<div class="signup clearfix">--}}
+					{{--<p>{{ trans('user.newsletter_sign_up') }}</p>--}}
+					{{--<form>--}}
+						{{--<input type="text" ng-model = "newsEmail"  class = "form-control input-sm" placeholder = "{{ trans('user.your_email_address_label') }}">--}}
+						{{--<input type="button" ng-click = "save()" value = "{{ trans('user.sign_up_label') }}">--}}
+					{{--</form>--}}
+				{{--</div>--}}
+			{{--@endif--}}
+		{{--</div>--}}
+
 
 	</div>
 
-	<div class="row credits">
-		<div class="col-md-12">
-			{{ trans('globals.power_by_label') }}&nbsp;<a href="http://antvel.com">{{ trans('globals.antvel_eCommerce') }}</a>
-		</div>
-	</div>
+	{{--<div class="row credits">--}}
+		{{--<div class="col-md-12">--}}
+			{{--{{ trans('globals.power_by_label') }}&nbsp;<a href="http://antvel.com">{{ trans('globals.antvel_eCommerce') }}</a>--}}
+		{{--</div>--}}
+	{{--</div>--}}
 
 </div>
 
