f978500d888dffb3bcfeee7b8e0c8d5c2b60da22
jonecherry <junchen.unif@qq.com>
2016-12-06 21:23:23 +0800

    修改contact.blade.php

--- resources/views/about/contact.blade.php
+++ resources/views/about/contact.blade.php
@@ -4,7 +4,7 @@
 
 {{-- NAVBAR --}}
 @section('navigation')
-  @include('partial.navigation_basic')
+  @include('partial.navigation')
 @endsection
 
 {{-- CONTENT --}}
@@ -14,16 +14,19 @@
     <div class="panel panel-default">
 
       <div class="panel-heading clearfix">
-        <h4 class="panel-title pull-left" style="padding-top: 7.5px;"><span class="glyphicon glyphicon-envelope"></span> {{trans('about.contact')}}</h4>
+        <h4 class="panel-title pull-left" style="padding-top: 7.5px;"><span class="glyphicon glyphicon-envelope"></span> 联系我们</h4>
+      </div>
+
+      <div class="product-img-box" >
+
+        <img  src='/img/qrcode_milan.jpg'>
+
       </div>
       <div class="panel-body">
-        <div class="col-sm-12 col-md-12 col-lg-6">
-          @include('about.info')
-          @include('about.map')
-        </div>
-        <div class="col-sm-12 col-md-12 col-lg-6">
-          @include('about.contact_form')
-        </div>
+        <h4>米兰居家生活馆</h4>
+        <h5>电话 ：18606797079</h5>
+        <h5>地址 ：浙江省 东阳市 通江路140号（锦豪大酒店斜对面）</h5>
+        <h5>主要经营 ：窗帘、壁纸、软包</h5>
       </div>
     </div>
   </div>
